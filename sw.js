2270c983cd6e23d4bc8a7ebba9bb26e286042ea0
leonardo rodriguez <leorodr4446@gmail.com>
2026-01-04 19:57:06 -0300

    versiontest1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "AGUA-v1";
+/*const CACHE_NAME = "AGUA-v1";
 const urlsToCache = [
   "/",
   "/index.html",
@@ -22,4 +22,4 @@ self.addEventListener("fetch", event => {
       return response || fetch(event.request);
     })
   );
-});
+});*/
